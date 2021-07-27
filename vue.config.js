73a750b3b4a0b0ff509d572e3a67e781354e5892
joker <le_yemeng@163.com>
2021-07-27 23:31:45 +0800

    feat: 为sass提供全局样式，以及全局变量

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devServer: {
+    open: true,
+    port: 9527,
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "./src/styles/var.scss";
+        `,
+      },
+    },
+  },
+};
